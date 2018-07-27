77763df2e09748cd9e88313f55c5554b376f2728
Andreas.Abdi <andreasabdi@yahoo.ca>
2018-07-26 23:34:30 -0700

    Added comments for functions and notes on general channel workflow

--- Channel.go
+++ Channel.go
@@ -30,9 +30,18 @@ type hasRequestId interface {
 	getRequestId() int
 }
 
-func (c *Channel) message(message *api.CastMessage, headers *PayloadHeaders) {
+/*
+	General workflow is
+	1. message is sent via the request method with unique requestID. adds an inflight chan to wait for event to return.
+	2. request method calls the send method and wraps the call around some stuff.
+	3. Client processes events in its active socket stream. If the requestID matches previous one then it sends the
+	unmarshalled message to the message function.
+	4. message function processes the event and passes to inflight chan. Also calls any attached listener functions.
+	5. Request method returns the unmarshalled chromecast event if it worked, timeout if it didn't receive the event in time.
+*/
 
-	//	spew.Dump("XXX", message, c)
+//Processes message that has been received by the packetstream in the client.
+func (c *Channel) message(message *api.CastMessage, headers *PayloadHeaders) {
 
 	if *message.DestinationId != "*" && (*message.SourceId != c.DestinationId || *message.DestinationId != c.sourceId || *message.Namespace != c.namespace) {
 		return
@@ -62,10 +71,14 @@ func (c *Channel) message(message *api.CastMessage, headers *PayloadHeaders) {
 
 }
 
+//OnMessage adds a callback listener for a certain type of message with specified responseType
 func (c *Channel) OnMessage(responseType string, cb func(*api.CastMessage)) {
 	c.listeners = append(c.listeners, channelListener{responseType, cb})
 }
 
+/*
+Send creates a simple message to be sent by the client.
+*/
 func (c *Channel) Send(payload interface{}) error {
 
 	payloadJson, err := json.Marshal(payload)
@@ -86,6 +99,10 @@ func (c *Channel) Send(payload interface{}) error {
 	return c.client.Send(message)
 }
 
+/*
+Request sends a payload and returns the message the chromecast gives back.
+Throws an error if timeout has passed waiting for the message to be returned.
+*/
 func (c *Channel) Request(payload hasRequestId, timeout time.Duration) (*api.CastMessage, error) {
 
 	payload.setRequestId(c.counter.IncrementAndGet())
