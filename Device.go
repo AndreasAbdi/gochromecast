104005584c1a06fd7e9c573be72e1622d484746f
Andreas.Abdi <andreasabdi@yahoo.ca>
2018-08-01 20:14:39 -0700

    Modified device struct to depend on primitives package

--- Device.go
+++ Device.go
@@ -1,11 +1,14 @@
 package castv2
 
-import "net"
+import (
+	"net"
+
+	"github.com/AndreasAbdi/go-castv2/primitives"
+)
 
 //Device Object to run basic chromecast commands
 type Device struct {
-	host net.IP
-	port int
+	client primitives.Client
 }
 
 //NewDevice is constructor for Device struct
