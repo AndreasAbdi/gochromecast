fc726925cbb6664963e599a9355789de08d4c2ef
Andreas.Abdi <andreasabdi@yahoo.ca>
2018-09-19 12:41:41 -0700

    Continued to move the examples so that we can test them faster later

--- examples/DisplayPicture/Picture.go
+++ examples/DisplayPicture/Picture.go
@@ -1,6 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"time"
+
+	"github.com/AndreasAbdi/go-castv2"
+)
+
 // A simple example, showing how to create a device and use it.
 func main() {
+	deviceCh := make(chan *castv2.Device, 100)
+	castv2.FindDevices(time.Second*30, deviceCh)
+	for device := range deviceCh {
+		fmt.Print(device)
+		device.PlayMedia(
+			"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/BigBuckBunny.jpg",
+			"image/jpeg")
+	}
 
 }
