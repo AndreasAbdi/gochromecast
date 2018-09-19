59873f109697a91aeafc5ec43e2a0c3a9277e4f5
Andreas.Abdi <andreasabdi@yahoo.ca>
2018-09-19 14:11:58 -0700

    Switched to returning device elements

--- DeviceFinder.go
+++ DeviceFinder.go
@@ -7,20 +7,21 @@ import (
 	"github.com/hashicorp/mdns"
 )
 
+//Hard defined device buffer size for
 //Chromecasts are chatty so we wouldn't need to worry too much about lots of devices in one network. It's not really feasible.
 const deviceBufferSearchSize = 100
 
 //FindDevices searches the LAN for chromecast devices via mDNS and sends them to a channel.
-func FindDevices(timeout time.Duration, devices chan<- Device) {
+func FindDevices(timeout time.Duration, devices chan<- *Device) {
 
 	// Make a channel for results and start listening
 	entries := make(chan *mdns.ServiceEntry, deviceBufferSearchSize)
 
+	go lookupChromecastMDNSEntries(entries, timeout)
 	go createDeviceObjects(entries, devices)
-	go lookupChromecastMDNSENtries(entries, timeout)
 }
 
-func createDeviceObjects(entries <-chan *mdns.ServiceEntry, devices chan<- Device) {
+func createDeviceObjects(entries <-chan *mdns.ServiceEntry, devices chan<- *Device) {
 	defer close(devices)
 	for entry := range entries {
 		if !strings.Contains(entry.Name, chromecastServiceName) {
@@ -30,10 +31,10 @@ func createDeviceObjects(entries <-chan *mdns.ServiceEntry, devices chan<- Devic
 		if err != nil {
 			return
 		}
-		devices <- device
+		devices <- &device
 	}
 }
-func lookupChromecastMDNSENtries(entries chan<- *mdns.ServiceEntry, timeout time.Duration) {
+func lookupChromecastMDNSEntries(entries chan<- *mdns.ServiceEntry, timeout time.Duration) {
 	defer close(entries)
 	mdns.Query(&mdns.QueryParam{
 		Service: chromecastServiceName,
