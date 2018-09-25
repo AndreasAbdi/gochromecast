c2ac7045eedb14b3ca8b6e945b37a8e76cf6cf3b
Andreas.Abdi <andreasabdi@yahoo.ca>
2018-09-24 19:11:13 -0700

    Added basic example of controls with device object

--- examples/Basic/main.go
+++ examples/Basic/main.go
@@ -1,69 +1,19 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"os"
-	"os/signal"
-	"strings"
 	"time"
 
-	"github.com/AndreasAbdi/go-castv2/controllers"
-	"github.com/AndreasAbdi/go-castv2/primitives"
-	"github.com/davecgh/go-spew/spew"
-	"github.com/hashicorp/mdns"
+	castv2 "github.com/AndreasAbdi/go-castv2"
 )
 
 // A simple example, showing how to find a Chromecast using mdns, and request its status.
 func main() {
-
-	castService := "_googlecast._tcp"
-
-	// Make a channel for results and start listening
-	entriesCh := make(chan *mdns.ServiceEntry, 4)
-	go func() {
-		for entry := range entriesCh {
-
-			if !strings.Contains(entry.Name, castService) {
-				return
-			}
-
-			fmt.Printf("Got new chromecast: %v\n", entry)
-
-			client, err := primitives.NewClient(entry.Addr, entry.Port)
-
-			if err != nil {
-				log.Fatalf("Failed to connect to chromecast %s", entry.Addr)
-			}
-
-			//_ = controllers.NewHeartbeatController(client, "Tr@n$p0rt-0", "Tr@n$p0rt-0")
-
-			heartbeat := controllers.NewHeartbeatController(client, "sender-0", "receiver-0")
-			heartbeat.Start()
-
-			connection := controllers.NewConnectionController(client, "sender-0", "receiver-0")
-			connection.Connect()
-
-			receiver := controllers.NewReceiverController(client, "sender-0", "receiver-0")
-
-			response, err := receiver.GetStatus(time.Second * 5)
-
-			spew.Dump("Status response", response, err)
-		}
-	}()
-
-	go func() {
-		mdns.Query(&mdns.QueryParam{
-			Service: castService,
-			Timeout: time.Second * 30,
-			Entries: entriesCh,
-		})
-	}()
-
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt, os.Kill)
-
-	// Block until a signal is received.
-	s := <-c
-	fmt.Println("Got signal:", s)
+	devices := make(chan *castv2.Device, 100)
+	castv2.FindDevices(time.Second*5, devices)
+	for device := range devices {
+		device.PlayMedia("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4", "video/mp4")
+		time.Sleep(time.Second * 5)
+		device.MediaController.Pause(time.Second * 5)
+		device.QuitApplication(time.Second * 5)
+	}
 }
